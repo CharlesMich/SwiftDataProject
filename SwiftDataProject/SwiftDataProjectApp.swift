f240c5b0d675a0dfe4e47e663c1f1e1f6be847cb
CharlesMich <charleedongo@hotmail.com>
2024-11-12 13:02:16 -0600

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Charles Michael on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
